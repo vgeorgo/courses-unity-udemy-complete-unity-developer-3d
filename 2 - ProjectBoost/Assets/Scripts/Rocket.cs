ad159a9ea998e86d0af5656705546226fc43a4b3
Victor Oliveira <victor.oliveira@phdesign.com.br>
2018-09-05 19:17:07 -0300

    Ship thrust and controls logic

---
+++ 2 - ProjectBoost/Assets/Scripts/Rocket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    protected Rigidbody _Rigidbody;
+
+    void Start ()
+    {
+        _Rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	void Update ()
+    {
+        ProcessInput();
+	}
+
+    protected void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            _Rigidbody.AddRelativeForce(Vector3.up);
+        }
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+
+        }
+    }
+}
