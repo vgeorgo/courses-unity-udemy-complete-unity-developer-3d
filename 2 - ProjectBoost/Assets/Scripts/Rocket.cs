a066cd85d089ffdc8eea7cd0de51bb838a45ad05
Victor Oliveira <victor.oliveira@phdesign.com.br>
2018-09-11 13:47:44 -0300

    Obstacles and collision detection

--- 2 - ProjectBoost/Assets/Scripts/Rocket.cs
+++ 2 - ProjectBoost/Assets/Scripts/Rocket.cs
@@ -5,48 +5,76 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    protected Rigidbody _Rigidbody;
-    protected AudioSource _AudioSource;
-    protected bool _IsPlayingEngine;
+    protected Rigidbody _rigidbody;
+    protected AudioSource _audioSource;
+
+    [SerializeField] float Thrust = 250f;
+    [SerializeField] float RcsThrust = 250f;
 
     void Start ()
     {
-        _Rigidbody = GetComponent<Rigidbody>();
-        _AudioSource = GetComponent<AudioSource>();
+        _rigidbody = GetComponent<Rigidbody>();
+        _audioSource = GetComponent<AudioSource>();
     }
 	
 	void Update ()
     {
-        ProcessInput();
-	}
+        ProcessThrust();
+        ProcessRotation();
+    }
 
-    protected void ProcessInput()
+    void OnCollisionEnter(Collision collision)
     {
-        if(Input.GetKey(KeyCode.Space))
+        switch(collision.gameObject.tag)
         {
-            _Rigidbody.AddRelativeForce(Vector3.up);
+            case "Friendly":
 
-            if (!_AudioSource.isPlaying)
+                break;
+
+            case "Fuel":
+
+                break;
+
+            default:
+
+                break;
+        }
+    }
+
+    protected void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _rigidbody.AddRelativeForce(Vector3.up * Time.deltaTime * Thrust);
+
+            if (!_audioSource.isPlaying)
             {
-                _AudioSource.Play();
+                _audioSource.Play();
             }
         }
         else
         {
-            if (_AudioSource.isPlaying)
+            if (_audioSource.isPlaying)
             {
-                _AudioSource.Stop();
+                _audioSource.Stop();
             }
         }
+    }
+
+    protected void ProcessRotation()
+    {
+        _rigidbody.freezeRotation = true; // take manual control of rotation
 
         if(Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * Time.deltaTime * RcsThrust);
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * Time.deltaTime * RcsThrust);
         }
+
+        _rigidbody.freezeRotation = false; // resume physics control of rotation
     }
 }
