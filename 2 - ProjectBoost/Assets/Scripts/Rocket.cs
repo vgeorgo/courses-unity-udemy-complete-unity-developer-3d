e8193bcba7e2a827a68f6e71977e2438e0d57180
Victor Oliveira <victor.oliveira@phdesign.com.br>
2018-09-11 12:52:07 -0300

    Controllers and sfx

--- 2 - ProjectBoost/Assets/Scripts/Rocket.cs
+++ 2 - ProjectBoost/Assets/Scripts/Rocket.cs
@@ -6,11 +6,14 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     protected Rigidbody _Rigidbody;
+    protected AudioSource _AudioSource;
+    protected bool _IsPlayingEngine;
 
     void Start ()
     {
         _Rigidbody = GetComponent<Rigidbody>();
-	}
+        _AudioSource = GetComponent<AudioSource>();
+    }
 	
 	void Update ()
     {
@@ -22,16 +25,28 @@ public class Rocket : MonoBehaviour
         if(Input.GetKey(KeyCode.Space))
         {
             _Rigidbody.AddRelativeForce(Vector3.up);
+
+            if (!_AudioSource.isPlaying)
+            {
+                _AudioSource.Play();
+            }
+        }
+        else
+        {
+            if (_AudioSource.isPlaying)
+            {
+                _AudioSource.Stop();
+            }
         }
 
         if(Input.GetKey(KeyCode.LeftArrow))
         {
-
+            transform.Rotate(Vector3.forward);
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
