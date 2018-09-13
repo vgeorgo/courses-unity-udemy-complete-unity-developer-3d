9b8fe887305fd0fab5c37b9a7f2b6d428355c3cb
Victor Oliveira <victor.oliveira@phdesign.com.br>
2018-09-13 13:24:37 -0300

    Particles, sfx, light fixes

---
+++ 2 - ProjectBoost/Assets/Scripts/Oscillator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+	    
+	}
+}
--- 2 - ProjectBoost/Assets/Scripts/Rocket.cs
+++ 2 - ProjectBoost/Assets/Scripts/Rocket.cs
@@ -3,11 +3,22 @@ using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
+    enum State { Alive, Dying, Transcending }
+    State _state = State.Alive;
+
     protected Rigidbody _rigidbody;
     protected AudioSource _audioSource;
 
-    [SerializeField] float Thrust = 250f;
-    [SerializeField] float RcsThrust = 250f;
+    [SerializeField] float thrust = 250f;
+    [SerializeField] float rcsThrust = 250f;
+    [SerializeField] float delayScene = 2f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip success;
+    [SerializeField] AudioClip death;
+
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem successParticles;
+    [SerializeField] ParticleSystem deathParticles;
 
     void Start ()
     {
@@ -17,12 +28,18 @@ public class Rocket : MonoBehaviour
 	
 	void Update ()
     {
-        ProcessThrust();
-        ProcessRotation();
+        if(_state == State.Alive)
+        {
+            ProcessThrust();
+            ProcessRotation();
+        }
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        if (_state != State.Alive)
+            return;
+
         switch(collision.gameObject.tag)
         {
             case "Friendly":
@@ -34,33 +51,64 @@ public class Rocket : MonoBehaviour
                 break;
 
             case "Finish":
-                SceneManager.LoadScene(1);
+                StartSuccessSequence();
                 break;
 
             default:
-                SceneManager.LoadScene(0);
+                StartDeathSequence();
                 break;
         }
+    }
+
+    private void StartSuccessSequence()
+    {
+        _state = State.Transcending;
+        _audioSource.Stop();
+        _audioSource.PlayOneShot(success);
+        successParticles.Play();
+        Invoke("LoadNextLevel", 1f);
+    }
+
+    private void StartDeathSequence()
+    {
+        _state = State.Dying;
+        _audioSource.Stop();
+        _audioSource.PlayOneShot(death);
+        deathParticles.Play();
+        Invoke("LoadFirstLevel", 1f);
+    }
+
+    protected void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    protected void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
     }
 
     protected void ProcessThrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            _rigidbody.AddRelativeForce(Vector3.up * Time.deltaTime * Thrust);
-
-            if (!_audioSource.isPlaying)
-            {
-                _audioSource.Play();
-            }
+            ApplyThrust();
         }
         else
         {
-            if (_audioSource.isPlaying)
-            {
-                _audioSource.Stop();
-            }
+            _audioSource.Stop();
+            mainEngineParticles.Stop();
         }
+    }
+
+    protected void ApplyThrust()
+    {
+        _rigidbody.AddRelativeForce(Vector3.up * Time.deltaTime * thrust);
+        if (!_audioSource.isPlaying)
+            _audioSource.PlayOneShot(mainEngine);
+
+        if(!mainEngineParticles.isEmitting)
+            mainEngineParticles.Play();
     }
 
     protected void ProcessRotation()
@@ -69,12 +117,12 @@ public class Rocket : MonoBehaviour
 
         if(Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Rotate(Vector3.forward * Time.deltaTime * RcsThrust);
+            transform.Rotate(Vector3.forward * Time.deltaTime * rcsThrust);
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Rotate(-Vector3.forward * Time.deltaTime * RcsThrust);
+            transform.Rotate(-Vector3.forward * Time.deltaTime * rcsThrust);
         }
 
         _rigidbody.freezeRotation = false; // resume physics control of rotation
