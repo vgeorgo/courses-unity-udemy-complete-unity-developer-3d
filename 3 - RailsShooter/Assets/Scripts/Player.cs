2664f59dd78be20fb7f3d99dbb6cec7bf61f1434
Victor Oliveira <victor.oliveira@phdesign.com.br>
2018-09-20 20:13:11 -0300

    Player movement

--- 3 - RailsShooter/Assets/Scripts/Player.cs
+++ 3 - RailsShooter/Assets/Scripts/Player.cs
@@ -3,13 +3,57 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
-	void Start ()
+    [Tooltip("In m/s")][SerializeField] float xSpeed = 4f;
+    [Tooltip("In m")][SerializeField] float xRange = 2f;
+    [Tooltip("In m")][SerializeField] float yRange = 1f;
+
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float controlPitchFactor = -25f;
+
+    [SerializeField] float positionYawFactor = 6f;
+
+    [SerializeField] float positionRollFactor = -20f;
+
+    float xThrow, yThrow;
+
+    void Start ()
     {
 		
 	}
 	
 	void Update ()
     {
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-	}
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * xSpeed * Time.deltaTime;
+
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
+
+    void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+
+        float yaw = transform.localPosition.x * positionYawFactor;
+
+        float roll = xThrow * positionRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
 }
