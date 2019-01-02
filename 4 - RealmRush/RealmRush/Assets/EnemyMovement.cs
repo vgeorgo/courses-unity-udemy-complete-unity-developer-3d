85dfe27c26ad14d03dabbb3aef3d6bbac82a99cc
Victor Oliveira <victor.oliveira@phdesign.com.br>
2019-01-02 17:42:19 -0200

    Grid snaping and labeling

---
+++ 4 - RealmRush/RealmRush/Assets/EnemyMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] List<Block> path;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    protected void PrintWaypoints()
+    {
+        foreach (Block waypoint in path)
+            print(waypoint.name);
+    }
+}
---
+++ 4 - RealmRush/RealmRush/Assets/Scripts/CubeEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
+{
+    [SerializeField] [Range(1f,20f)] float gridSize = 10f;
+
+    TextMesh textMesh;
+
+    void Update()
+    {
+        textMesh = GetComponentInChildren<TextMesh>();
+        Vector3 snapPos;
+
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+        gameObject.name = textMesh.text = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+    }
+}
